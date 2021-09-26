29d90141bdcce3713801affdb6c330879ba6946a
ececi <huseyinkiceci@gmail.com>
2021-09-27 00:23:29 +0300

    TC8 solution finished

--- src/test/java/TC8.java
+++ src/test/java/TC8.java
@@ -3,9 +3,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -44,5 +46,39 @@ public class TC8 {
         driver.quit();
     }
 
+    @Test
+    void test(){
+/*1. Go to "http://cyberkings.kicchi.net/AutomizationPortal.html"
+2. Select "TV" checkbox
+3. Verify h5 element is shown below checkboxes with "TV" text.
+4. Select "Console" checkbox
+5. Verify h5 element is shown below checkboxes with "TV + Console" text.
+6. Unselect "TV" checkbox
+7. Verify h5 element is shown below checkboxes with "Console" text.
+8. Select "Smart Phone" checkbox
+9. Verify h5 element is shown below checkboxes with "Console + Smart Phone" text
+10 Select "TV" checkbox
+11. Verify h5 element is shown below checkboxes with "TV + Console + Smart Phone" text*/
+
+        cbTV.click();
+        checkVisibleText( "TV");
+
+        cbConsole.click();
+        checkVisibleText( "TV + Console");
+
+        cbTV.click();
+        checkVisibleText( "Console");
+
+        cbSmartPhone.click();
+        checkVisibleText( "Console + Smart Phone");
+
+        cbTV.click();
+        checkVisibleText( "TV + Console + Smart Phone");
+    }
+
+    private void checkVisibleText(String expectedText){
+        Assert.assertTrue(messageElement.isDisplayed());
+        Assert.assertEquals(messageElement.getText(), expectedText);
+    }
 
 }
