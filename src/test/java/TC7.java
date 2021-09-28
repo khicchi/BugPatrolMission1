e7994fa9ec11dd3bf12f211b3314083b9836a7e9
Archie-f <akif.mk66@gmail.com>
2021-09-28 19:45:49 +0300

    Created Before and After methods.

---
+++ src/test/java/TC7.java
@@ -0,0 +1,44 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TC7 {
+
+    /*
+    1. Go to "http://cyberkings.kicchi.net/AutomizationPortal.html"
+    2. Click "Submit" button
+    3. Verify "No car selected" text is displayed
+    4. Click "Fiat" from the dropdown list
+    5. Click "Submit" button
+    6. Verify "Fiat selected" text is displayed
+    */
+
+    WebElement[][] labels = null;
+    WebDriver driver;
+
+    @BeforeClass
+    void setupClass(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    void setupMethod() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://cyberkings.kicchi.net/AutomizationPortal.html");
+    }
+
+    @AfterMethod
+    void tearDown(){
+        driver.quit();
+    }
+
+
+}
