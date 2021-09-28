8b46c4e2abe1f8b6b428a17869ecc2f3a0923d5e
Archie-f <akif.mk66@gmail.com>
2021-09-28 20:52:22 +0300

    Started coding testCase7

--- src/test/java/TC7.java
+++ src/test/java/TC7.java
@@ -1,10 +1,14 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -40,5 +44,13 @@ public class TC7 {
         driver.quit();
     }
 
+    @Test
+    public void testCase7(){
+        //2. Click "Submit" button
+        driver.findElement(By.cssSelector("*[type='submit']")).click();
+
+
+
+    }
 
 }
