67905d53a904c1f972b4e285c5ba39ae093f2c95
abdullahozdemir <a20ozdemir@gmail.com>
2021-09-29 23:46:11 +0300

    TCO3 Request take off :)

---
+++ src/test/java/TC3azod.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC3azod {
+
+
+@Test
+public void test3(){
+/*1. Go to "http://cyberkings.kicchi.net/AutomizationPortal.html"
+2. Click on the "Generate random number" button
+3. Type a different number from the generated number into the textbox below the
+number
+4. Click on the "Verify number" button
+5. Verify that a red cross image is displayed
+ */
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+
+}
+}
