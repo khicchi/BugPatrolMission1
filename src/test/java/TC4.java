41757919cc02b55c0dbcca739f96e588a3ae362b
orhanharman <kartal1835@gmail.com>
2021-09-29 11:56:50 +0300

    TC4 is completed

--- src/test/java/TC4.java
+++ src/test/java/TC4.java
@@ -1,6 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -8,6 +11,12 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class TC4 {
+    /*
+    1. Go to "http://cyberkings.kicchi.net/AutomizationPortal.html"
+    2. Click on the "Verify number" button
+    3. Verify a JS alert message is displayed.
+    4. Verify the alert message is "Generate random number first!"
+    */
 
     WebDriver driver;
 
@@ -21,25 +30,32 @@ public class TC4 {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        //1. Go to "http://cyberkings.kicchi.net/AutomizationPortal.html"
         driver.get("http://cyberkings.kicchi.net/AutomizationPortal.html");
     }
 
     @AfterMethod
     public void tearDown() throws InterruptedException {
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.quit();
     }
 
     @Test
-    public void TC4_Orhi(){
-    /*
-    1. Go to "http://cyberkings.kicchi.net/AutomizationPortal.html"
-    2. Click on the "Verify number" button
-    3. Verify a JS alert message is displayed.
-    4. Verify the alert message is "Generate random number first!"
-    */
+    public void TC4_Orhi() {
 
+        //2. Click on the "Verify number" button
+        driver.findElement(By.id("btnVerifyNumber")).click();
 
+        //3. Verify a JS alert message is displayed
+        Alert alert = driver.switchTo().alert();
+        String actualAlertMsg = alert.getText();
+        boolean alertVerifier = !(actualAlertMsg==null);
+        Assert.assertTrue(alertVerifier, "Verify alert message is displayed");
+
+        //4. Verify the alert message is "Generate random number first!"
+        String expectedAlertMsg = "Generate random number first!";
+        Assert.assertEquals(actualAlertMsg, expectedAlertMsg, "Verify the alert message");
 
     }
 
