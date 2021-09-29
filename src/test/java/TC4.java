62c5b2c7a174d3b5d244118671032fcf929ee4f4
orhanharman <kartal1835@gmail.com>
2021-09-29 09:43:18 +0300

    Template is ready for TC4

--- src/test/java/TC4.java
+++ src/test/java/TC4.java
@@ -1,5 +1,46 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
 public class TC4 {
 
+    WebDriver driver;
 
+    @BeforeClass
+    public void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("http://cyberkings.kicchi.net/AutomizationPortal.html");
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    @Test
+    public void TC4_Orhi(){
+    /*
+    1. Go to "http://cyberkings.kicchi.net/AutomizationPortal.html"
+    2. Click on the "Verify number" button
+    3. Verify a JS alert message is displayed.
+    4. Verify the alert message is "Generate random number first!"
+    */
+
+
+
+    }
 
 }
