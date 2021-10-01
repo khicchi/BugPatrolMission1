ead29e90db95e65b4b1116247d6e9c4ece200ffd
hilmi <hilmi11@qa.team>
2021-10-01 22:39:54 +0300

    I have started TC10

---
+++ src/test/java/TC10.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
+public class TC10 {
+
+@Test
+    public void TCHun10() throws InterruptedException {
+
+    WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+    driver.get("http://cyberkings.kicchi.net/AutomizationPortal.html");
+    WebElement optionButton= driver.findElement(By.id("radio9_24"));
+    optionButton.click();
+    Thread.sleep(2000);
+    optionButton.click();
+
+
+
+  }
+}
