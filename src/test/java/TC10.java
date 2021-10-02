71d8953338f7cb03c50c27ef24a7983e7c2b9280
hilmi <hilmi11@qa.team>
2021-10-02 10:05:11 +0300

    I have finished my TC

--- src/test/java/TC10.java
+++ src/test/java/TC10.java
@@ -1,15 +1,30 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import utilities.WebDriverFactory;
 
+import java.util.List;
+
 public class TC10 {
 
-@Test
-    public void TCHun10() throws InterruptedException {
+    WebDriver driver;
 
-    WebDriver driver = WebDriverFactory.getDriver("chrome");
+    @BeforeMethod
+        public void setup(){
+         driver = WebDriverFactory.getDriver("chrome");
+        }
+    @AfterMethod
+        public void close() throws InterruptedException {
+        Thread.sleep(1000);
+        driver.quit();
+    }
+
+    @Test
+    public void TCHun10() throws InterruptedException {
 
     driver.get("http://cyberkings.kicchi.net/AutomizationPortal.html");
     WebElement optionButton= driver.findElement(By.id("radio9_24"));
@@ -17,14 +32,11 @@ public class TC10 {
     Thread.sleep(2000);
     optionButton.click();
 
-    System.out.println("hello");
-    System.out.println("helloaa");
-    System.out.println("fadfaf");
+    List<WebElement> Texts = driver.findElements(By.xpath("//label[.='Gen Z']"));
+    Assert.assertEquals(Texts.size(),2);
 
-    System.out.println("helloaa");
-
+        System.out.println("There are "+Texts.size() +" \"Gen Z\" texts at the page");
 
 
-
-  }
+    }
 }
